3bb242ffe8b6f2193fdcd97ba13368139f412b1a
Taha Husain <taha.husain@weboniselab.com>
2015-02-04 19:20:04 +0530

    exercise 4 to 9 complete

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -35,22 +35,46 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)
+    )
+  )
 
 (defn height [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)
+    )
+  )
 
-(defn square? [rectangle]
-  :-)
+(defn square? [rect]
+  (let [[[x1 y1] [x2 y2]] rect
+    height (height (rectangle [x1 y1] [x2 y2]))
+    width (width (rectangle [x1 y1] [x2 y2]))]
+    (== height width)
+    )
+  )
 
 (defn area [rectangle]
-  :-)
+  (let [height (height rectangle)
+        width (width rectangle)]
+    (* height width)
+    )
+  )
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[x y] point
+        [[x1 y1] [x2 y2]] rectangle]
+    (and (<= x1 x x2) (<= y1 y y2))
+    )
+  )
 
 (defn contains-rectangle? [outer inner]
-  :-)
+  (let [[point1 point2] inner]
+    (and (contains-point? outer point1)
+         (contains-point? outer point2)
+      )
+    )
+  )
 
 (defn title-length [book]
   :-)
